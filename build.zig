432536929b6e73103273309c5c0b1fef49f6c67d
Jesse Rudolph <jesse.rudolph@gmail.com>
2020-05-22 02:09:51 -0500

    update for changes in raylib-zig submodule

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const Builder = std.build.Builder;
 
-// import raydep and tell it where raylib-zig is
-const raydep = @import("./raylib-zig/raydep.zig").Pkg("./raylib-zig");
+// import lib.zig and tell it where raylib-zig is
+const raylib = @import("./raylib-zig/lib.zig").Pkg("./raylib-zig");
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
@@ -22,7 +22,10 @@ pub fn build(b: *Builder) void {
     exe.setTarget(target);
 
     // this does most of the heavy lifting
-    raydep.addDeps(exe);
+    raylib.link(exe);
+    raylib.addAsPackage("raylib", exe);
+    raylib.math.addAsPackage("raylib-math", exe);
+
     const run_cmd = exe.run();
     const run_step = b.step("run", "Run the game");
     run_step.dependOn(&run_cmd.step);
